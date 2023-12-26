568b530d34b335cea0ffe146f945e6ac3e516a48
cerega <12345cerega@mail.ru>
2023-12-26 18:26:49 +0300

    fix bag

--- script.js
+++ script.js
@@ -4,40 +4,39 @@ let scicers = document.querySelector(".scicers");
 let result = document.querySelector(".result");
 let downloadButton = document.querySelector(".download-button");
 
-
 rock.addEventListener("click", function () {
-    playGame("Камень");
+  playGame("Камень");
 });
 
 paper.addEventListener("click", function () {
-    playGame("Бумага");
+  playGame("Бумага");
 });
 //asdasgit init
 scicers.addEventListener("click", function () {
-    playGame("Ножницы");
+  playGame("Ножницы");
 });
 
 function playGame(playerChoise) {
-    let choises = ["Камень", "Ножницы", "Бумага"];
-    let computerChoise = choises[Math.floor(Math.random() * 3)];
-    if (playerChoise == computerChoise) {
-        result.textContent = "Ничья";
-    } else if (
-        (playerChoise == "Камень" && computerChoise == "Ножницы") ||
-        (playerChoise == "Ножницы" && computerChoise == "Бумага") ||
-        (playerChoise == "Бумага" && computerChoise == "Камень")
-    ) {
-        result.textContent = "Вы выиграли комьютер выбрал " + computerChoise;
-    } else {
-        result.textContent = "Вы проиграли компьютер выбрал " + computerChoise;
-    }
+  let choises = ["Камень", "Ножницы", "Бумага"];
+  let computerChoise = choises[Math.floor(Math.random() * 3)];
+  if (playerChoise == computerChoise) {
+    result.textContent = "Ничья";
+  } else if (
+    (playerChoise == "Камень" && computerChoise == "Ножницы") ||
+    (playerChoise == "Ножницы" && computerChoise == "Бумага") ||
+    (playerChoise == "Бумага" && computerChoise == "Камень")
+  ) {
+    result.textContent = "Вы выиграли комьютер выбрал " + computerChoise;
+  } else {
+    result.textContent = "Вы проиграли компьютер выбрал " + computerChoise;
+  }
 }
 
 let defaultInstallEvent = null;
 window.addEventListener("beforeinstallprompt", (event) => {
-    event.preventDefault();
-    defaultInstallEvent = event;
+  event.preventDefault();
+  defaultInstallEvent = event;
 });
 downloadButton.addEventListener("click", (event) => {
-    defaultInstallEvent.prompt();
+  defaultInstallEvent.prompt();
 });
